d0b56fac195e59d2fa9d195fecc6bc325cb98b93
User1 <User1@DESKTOP-OE2HQO6>
2020-11-11 14:30:12 +0000

    Changed Hello method

--- src/main/java/com/mycompany/is3313minimaljavademo/NewClass.java
+++ src/main/java/com/mycompany/is3313minimaljavademo/NewClass.java
@@ -11,6 +11,6 @@ package com.mycompany.is3313minimaljavademo;
  */
 public class NewClass {
     public static void main (String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello IS3313");
     }
 }
